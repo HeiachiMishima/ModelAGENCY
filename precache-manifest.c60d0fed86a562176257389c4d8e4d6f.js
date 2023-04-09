c10227b8053ecdf475ab4f1c26b9899e02d445f2
AhsanKhanDev <ahsan@semanticsol.com>
2023-04-09 16:42:27 +0500

    MODEL AGENCY version 1.1

---
+++ precache-manifest.c60d0fed86a562176257389c4d8e4d6f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce99e82d5e497611791b",
+    "url": "/static/css/main.9521fab7.chunk.css"
+  },
+  {
+    "revision": "ce99e82d5e497611791b",
+    "url": "/static/js/main.ce99e82d.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "8130de09a5defc1a0cb2",
+    "url": "/static/js/2.8130de09.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "fdcb6687d0796d270bc32d1243bf76ec",
+    "url": "/static/media/Slider2.fdcb6687.png"
+  },
+  {
+    "revision": "131044dbac9094dfab5e67d8bfb625f1",
+    "url": "/static/media/Banner.131044db.png"
+  },
+  {
+    "revision": "6eb44fca5e394b6fd7e701ac2f4e5b0f",
+    "url": "/static/media/Model1.6eb44fca.jpg"
+  },
+  {
+    "revision": "bd4de7f6fba8691d42b7e2588cb9346d",
+    "url": "/static/media/Model2.bd4de7f6.jpg"
+  },
+  {
+    "revision": "a13252f4677c0e128531748a41ff6bad",
+    "url": "/static/media/Model3.a13252f4.jpg"
+  },
+  {
+    "revision": "240971006f900cd122501cbd8231ecd9",
+    "url": "/static/media/Slider.24097100.png"
+  },
+  {
+    "revision": "c9ddf19184726b03ece0bbadd4a651f8",
+    "url": "/static/media/team1.c9ddf191.jpg"
+  },
+  {
+    "revision": "cc4a260f8069836e539a498dbe30f6ad",
+    "url": "/static/media/team2.cc4a260f.jpg"
+  },
+  {
+    "revision": "0999f70f14c331b41f4a57f9b565eb79",
+    "url": "/static/media/team3.0999f70f.jpg"
+  },
+  {
+    "revision": "4e4083f616861b99f8fd678650e6db7c",
+    "url": "/static/media/team4.4e4083f6.jpg"
+  },
+  {
+    "revision": "08683c515d3dc8b6f4dec817c8f8d006",
+    "url": "/static/media/Akronim-Regular.08683c51.ttf"
+  },
+  {
+    "revision": "093ee89be9ede30383f39a899c485a82",
+    "url": "/static/media/poppinsregular.093ee89b.ttf"
+  },
+  {
+    "revision": "d805c3e4f798e3c69983fa937a5e56b8",
+    "url": "/static/media/Aladin-Regular.d805c3e4.ttf"
+  },
+  {
+    "revision": "ca399fd2286e6d1e50bd65bd950b33ef",
+    "url": "/static/media/Oswald.ca399fd2.ttf"
+  },
+  {
+    "revision": "19133052df4db638070f507314e71035",
+    "url": "/index.html"
+  }
+];
